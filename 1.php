7e4d820cbec0749a5bfc84b073d4a950548e1921
Sergey Makarov <wikendp@gmail.com>
2019-03-08 14:25:06 +0300

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,31 @@
+<?php
+    
+    function fibonacci($n){
+         $fib = array(0,1);
+         for($i = 0; $i < $n - 2; $i++){
+             $current = $fib[$i] + $fib[$i+1];
+             array_push($fib,$current);
+         }
+         
+         return implode("  ",$fib);
+    }
+    
+    function fibonacci_recursive($n) {
+        
+        if($n <=2)
+            return 1;
+        else {
+            return fibonacci_recursive($n-1) + fibonacci_recursive($n-2); 
+        }
+    }
+    
+    echo "simple function: " . fibonacci(20);
+    echo "\n========================================================\nrecursive function: ";
+    $i = 1;
+    while($i < 20) {
+        
+        echo fibonacci_recursive($i)."  ";
+        $i++;
+    }
+
+?>
